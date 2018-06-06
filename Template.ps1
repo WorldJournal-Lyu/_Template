c9928068f798a58b59e6cb5eb316a249b45da682
WorldJournal-Lyu <39061224+WorldJournal-Lyu@users.noreply.github.com>
2018-06-06 14:21:25 -0400

    First commit

---
+++ Template.ps1
@@ -0,0 +1,58 @@
+<#
+
+Template.ps1
+
+    2018-05-30 Initial Creation
+
+#>
+
+if (!($env:PSModulePath -match 'C:\\PowerShell\\_Modules')) {
+    $env:PSModulePath = $env:PSModulePath + ';C:\PowerShell\_Modules\'
+}
+
+Import-Module WorldJournal.Log -Verbose -Force
+Import-Module WorldJournal.Email -Verbose -Force
+Import-Module WorldJournal.Server -Verbose -Force
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptName = (($MyInvocation.MyCommand) -Replace ".ps1")
+$hasError   = $false
+
+$newlog     = New-Log -Path $scriptPath -LogFormat yyyyMMdd-HHmmss
+$log        = $newlog.FullName
+$logPath    = $newlog.Directory
+
+$mailFrom   = (Get-WJEmail -Name noreply).MailAddress
+$mailPass   = (Get-WJEmail -Name noreply).Password
+$mailTo     = (Get-WJEmail -Name lyu).MailAddress
+$mailSbj    = $scriptName
+$mailMsg    = ""
+
+Write-Log -Verb "LOG START" -Noun $log -Path $log -Type Long -Status Normal
+Write-Line -Length 100 -Path $log
+
+###################################################################################
+
+
+
+$newspage = (Get-WJPath -Name newspage).Path
+$mailMsg  = $mailMsg + ("NEWSPAGE: " + $newspage)
+Write-Log -Verb "NEWSPAGE" -Noun $newspage -Path $log -Type Long -Status Normal
+
+
+
+###################################################################################
+
+Write-Log -Verb "LOG END" -Noun $log -Path $log -Type Long -Status Normal
+if($hasError){ $mailSbj = "ERROR " + $scriptName }
+
+$emailParam = @{
+    From    = $mailFrom
+    Pass    = $mailPass
+    To      = $mailTo
+    Subject = $mailSbj
+    Body    = $scriptName + " completed at: " + (Get-Date).ToString("HH:mm:ss") + "`n`n" + $mailMsg
+    ScriptPath = $scriptPath
+    Attachment = $log
+}
+Emailv2 @emailParam
